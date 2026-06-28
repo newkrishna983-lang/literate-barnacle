2ebb2c7cd18997554a8d8a363d9c18d7858771b3
newkrishna983-lang <newkrishna983@gmail.com>
2026-06-28 08:06:42 +0530

    Change default values for environment variables
    Updated environment variable defaults for API_ID, API_HASH, BOT_TOKEN, OWNER_ID, and SUDO_USERS.

--- vars.py
+++ vars.py
@@ -3,9 +3,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID", 0))
-API_HASH = getenv("API_HASH", None)
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", 57))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "57").split()))
+API_ID = int(getenv("API_ID", 34422904))
+API_HASH = getenv("API_HASH", 7e0002469784f47fc08a6b3d93d7ebed)
+BOT_TOKEN = getenv("BOT_TOKEN", 8638398538:AAGd0NJuGoMEGbY2Zx7d1RHF8z6gaZ5PVo4)
+OWNER_ID = int(getenv("OWNER_ID", 5349573682))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5349573682").split()))
 
